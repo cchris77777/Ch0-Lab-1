d78fd42bca7a598ba6b6faed75d57b02ed535af2
unknown <cchris777777@gmail.com>
2018-09-25 22:04:25 +0800

    Add student ID

--- source/Main.c
+++ source/Main.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main(void) {
 	printf("Hello ,world!\n");
-	printf("My student ID is 107360208\n");
+	printf("My student ID is 107360208!\n");
 	system("pause");
 	return 0;
 }
